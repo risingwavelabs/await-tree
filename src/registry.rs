caacbf72d60ed9c3bc1d0f96148173d0821cb619
hillium <yu745514916@live.com>
2023-10-21 14:49:50 +0800

    move to weak-table

--- src/registry.rs
+++ src/registry.rs
@@ -13,14 +13,12 @@
 // limitations under the License.
 
 use std::borrow::Borrow;
-use std::collections::HashMap;
 use std::future::Future;
 use std::hash::Hash;
 use std::sync::{Arc, Weak};
-use std::time::Duration;
 
-use coarsetime::Instant;
 use derive_builder::Builder;
+use weak_table::WeakValueHashMap;
 
 use crate::context::{Tree, TreeContext, CONTEXT};
 use crate::Span;
@@ -32,19 +30,12 @@ use crate::Span;
 pub struct Config {
     /// Whether to include the **verbose** span in the await-tree.
     verbose: bool,
-    /// The minimal gap of triggering GC.
-    /// Generally GC will be triggered each time registering new futures to
-    /// the registry. (e.g. spawning new tasks to the runtime.)
-    gc_throttle_duration: Duration,
 }
 
 #[allow(clippy::derivable_impls)]
 impl Default for Config {
     fn default() -> Self {
-        Self {
-            verbose: false,
-            gc_throttle_duration: Duration::from_secs(1),
-        }
+        Self { verbose: false }
     }
 }
 
@@ -63,17 +54,18 @@ impl TreeRoot {
 /// The registry of multiple await-trees.
 #[derive(Debug)]
 pub struct Registry<K> {
-    contexts: HashMap<K, Weak<TreeContext>>,
-    last_gc: Instant,
+    contexts: WeakValueHashMap<K, Weak<TreeContext>>,
     config: Config,
 }
 
-impl<K> Registry<K> {
+impl<K> Registry<K>
+where
+    K: std::hash::Hash + Eq + std::fmt::Debug,
+{
     /// Create a new registry with given `config`.
     pub fn new(config: Config) -> Self {
         Self {
-            contexts: HashMap::new(),
-            last_gc: Instant::recent(),
+            contexts: WeakValueHashMap::new(),
             config,
         }
     }
@@ -88,20 +80,15 @@ where
     /// If the key already exists, a new [`TreeRoot`] is returned and the reference to the old
     /// [`TreeRoot`] is dropped.
     pub fn register(&mut self, key: K, root_span: impl Into<Span>) -> TreeRoot {
-        self.maybe_gc();
-
         let context = Arc::new(TreeContext::new(root_span.into(), self.config.verbose));
-        let weak = Arc::downgrade(&context);
-        self.contexts.insert(key, weak);
+        self.contexts.insert(key, Arc::clone(&context));
 
         TreeRoot { context }
     }
 
     /// Iterate over the clones of all registered await-trees.
     pub fn iter(&self) -> impl Iterator<Item = (&K, Tree)> {
-        self.contexts
-            .iter()
-            .filter_map(|(k, v)| v.upgrade().map(|v| (k, v.tree().clone())))
+        self.contexts.iter().map(|(k, v)| (k, v.tree().clone()))
     }
 
     /// Get a clone of the await-tree with given key.
@@ -112,46 +99,11 @@ where
         K: Borrow<Q>,
         Q: Hash + Eq,
     {
-        self.contexts
-            .get(k)
-            .and_then(|v| v.upgrade())
-            .map(|v| v.tree().clone())
+        self.contexts.get(k).map(|v| v.tree().clone())
     }
 
     /// Remove all the registered await-trees.
     pub fn clear(&mut self) {
         self.contexts.clear();
-    }
-
-    fn maybe_gc(&mut self) {
-        Instant::update();
-        let since_last_gc = self.last_gc.elapsed_since_recent();
-        if since_last_gc > self.config.gc_throttle_duration.into() {
-            // TODO: make this more efficient
-            self.contexts.retain(|_, v| v.upgrade().is_some());
-            self.last_gc = Instant::recent();
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::time::Duration;
-
-    use crate::Registry;
-
-    #[test]
-    fn test_delay_gc() {
-        let mut reg = Registry::new(crate::Config {
-            verbose: false,
-            gc_throttle_duration: Duration::from_millis(100),
-        });
-        for i in 0..100 {
-            let _ = reg.register(i, "gc_me");
-        }
-        assert_eq!(reg.contexts.len(), 100);
-        std::thread::sleep(Duration::from_millis(100));
-        reg.register(101, "not_yet_to_gc_me");
-        assert_eq!(reg.contexts.len(), 1);
     }
 }
